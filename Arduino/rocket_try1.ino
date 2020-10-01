bb9715dbf1425d96501a3a7cde9f8befde8b4294
emilymsm <71347007+emilymsm@users.noreply.github.com>
2020-10-01 13:23:39 -0700

    Add files via upload
    Code for rocket breadboard practice (red, red, green flashing lights)

---
+++ Arduino/rocket_try1.ino
@@ -0,0 +1,36 @@
+
+
+int switchState = 0;
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+ 
+}
+
+void loop() {
+  switchState = digitalRead(2); 
+  if (switchState == LOW) {
+    // the button is not pressed
+
+    digitalWrite(3, HIGH); // green LED
+    digitalWrite(4, LOW); // red LED
+    digitalWrite(5, LOW); //red LED
+  
+}
+
+else { //the button is pressed
+  digitalWrite(3, LOW); //green LED
+  digitalWrite(4, LOW); //red LED
+  digitalWrite(5, HIGH); //red LED
+
+
+  delay(250); // wait for a quarter second
+  //toggle the LEDs
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  delay(250); // write for a quarter second
+}
+
+} // go back to the beginning of the loop
